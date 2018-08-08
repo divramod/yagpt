e87e47d2a8bc168978e7a161e28d0f8f746ed98d
Arvid Petermann <arvidpetermann@gmail.com>
2018-08-08 12:17:04 +0200

    add bin (#1)

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+require('shelljs/global');
+
 exports.printMsg = function() {
   console.log("This is a message from the demo package");
 }
+
+let test = pwd().toString()
+console.log(test);
 
 console.log("hello world");
